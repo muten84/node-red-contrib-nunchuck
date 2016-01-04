2896076dad02debcf255c085405ed20053600cf4
Luigi Bifulco <bifulcoluigi@gmail.com>
2016-01-04 16:49:13 +0100

    decode and parse all data
    decode and parse all data

--- nunchuck-device.js
+++ nunchuck-device.js
@@ -29,31 +29,86 @@ NunchuckDevice.prototype.start = function(ondata){
   this.started = setInterval(function(){
     device.bus.i2cWriteSync(device.address, 1, new Buffer([0x00]));
     device.bus.i2cReadSync(device.address, 6, buffer);
-    var decoded = device.decode(buffer);
+    var decoded = device.decode(device.parseData(buffer));
     ondata(decoded);
   },device.frequency||1);
+
+  this.parseData = function(buffer){
+    var parsed = new Buffer(7);
+    parsed[0] = buffer[0]; //x
+    parsed[1] = buffer[1]; //y
+    parsed[2] = (~buffer[5]) & 0b1; //Z button
+    parsed[3] = ((~buffer[5])>>1) & 0b1; // C button
+    parsed[4] = (((~buffer[5])>>2) & 0b1) | (buffer[2]<<2) //aX;
+    parsed[5] = (((~buffer[5])>>4) & 0b1) | (buffer[3]<<2) //aY;
+    parsed[6] = (((~buffer[5])>>6) & 0b1) | (buffer[4]<<2) //aZ;
+    return parsed;
+	  /*n->X = nunchuk_data_buffer[0];
+	  n->Y = nunchuk_data_buffer[1];
+	  n->Z = (~nunchuk_data_buffer[5]) & 0b1;
+	  n->C = ((~nunchuk_data_buffer[5])>>1) & 0b1;
+	  n->aX = (((nunchuk_data_buffer[5]>>2) & 0b11) | ((int)nunchuk_data_buffer[2])<<2);
+	  n->aY = (((nunchuk_data_buffer[5]>>4) & 0b11) | ((int)nunchuk_data_buffer[3])<<2);
+	  n->aZ = (((nunchuk_data_buffer[5]>>6) & 0b11) | ((int)nunchuk_data_buffer[4])<<2);*/
+  }
 
   this.decode = function(buffer){
     var value = -1;
     //x -y..etc
     var i = -1;
-    var values = [lastXEvent,'Y','aX','aY','aZ'];
+    var values = [lastXEvent,lastYEvent,'aX','aY','aZ'];
     //buffer
     for(i=0; i<buffer.length; i++){
       value = buffer.readUInt8(i);
       switch(i){
         case 0:
-          values[0] = device.decodeX(value);
+          values[i] = device.decodeX(value);
           break;
         case 1:
-          values[1] = device.decodeY(value);
+          values[i] = device.decodeY(value);
+          break;
+        case 2:
+          values[i] = device.decodeC(value);
+          break;
+        case 3:
+          values[i] = device.decodeZ(value);
+          break;
+        case 4:
+          values[i] = device.decodeAx(value);
+          break;
+        case 5:
+          values[i] = device.decodeAy(value);
+          break;
+        case 6:
+          values[i] = device.decodeAz(value);
           break;
         default:
-        break;
+          break;
       }
     }
     return values;
   }
+
+  this.decodeC = function(newC){
+    return "idle";
+  }
+
+  this.decodeZ = function(newZ){
+    return "idle";
+  }
+
+  this.decodeAx= function(newAx){
+    return "medium";
+  }
+
+  this.decodeAy= function(newAy){
+    return "medium";
+  }
+
+  this.decodeAz= function(newAz){
+    return "medium";
+  }
+
 
   this.decodeY = function(newY){
     var diffY = newY - (center+(device.threshholdY||def_threshholdY));
