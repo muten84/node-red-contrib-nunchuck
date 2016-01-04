27ad196fd29a55e9e47a3236bf57ec922319c8e9
Luigi Bifulco <bifulcoluigi@gmail.com>
2016-01-04 22:24:58 +0100

    fix
    fix

--- main.js
+++ main.js
@@ -3,13 +3,14 @@ var NunchuckDevice = require('./nunchuck-device');
   var NUNCHUCK_ADDRESS = 0x52;
   var center = 128;
   var threshholdX = 142-center;
+  var threshholdY = 138-center;
   var currentX = center;
   var lastXEvent = "";
 
 
-var nunchuck = new NunchuckDevice(NUNCHUCK_ADDRESS, 1,[threshholdX]);
+var nunchuck = new NunchuckDevice(NUNCHUCK_ADDRESS, 1,[threshholdX,threshholdY]);
 nunchuck.init();
     nunchuck.start(function(stream){
-        
+
      console.log(stream);
     });
