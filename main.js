2087b5bbcaf1c8330ff34a35caa608948cb1f587
Luigi Bifulco <bifulcoluigi@gmail.com>
2016-01-05 17:39:42 +0100

    decode angles and tilts
    decode angles and tilts

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-var streamBuffers = require('stream-buffers');
-var Readable = require('stream').Readable;
 var NunchuckDevice = require('./nunchuck-device');
+
 
 var NUNCHUCK_ADDRESS = 0x52;
 var center = 128;
@@ -8,22 +7,15 @@ var threshholdX = 142-center;
 var threshholdY = 138-center;
 var currentX = center;
 var lastXEvent = "";
+var idx = 0;
 
 
-var nunchuck = new NunchuckDevice(NUNCHUCK_ADDRESS, 1,[threshholdX,threshholdY]);
+var nunchuck = new NunchuckDevice(NUNCHUCK_ADDRESS, 10,[threshholdX,threshholdY]);
 nunchuck.init();
-var axStream = new streamBuffers.ReadableStreamBuffer({
-	frequency: 1,   // in milliseconds.
-	chunkSize: 1  // in bytes.
-});
-axStream.on('readable', function(data) {
-  var chunk;
-  while((chunk = axStream.read()) !== null) {
-    console.log("length: "+chunk.length);
-    console.log(chunk.readInt8(0));
-  }
-});
+
 nunchuck.start(function(stream){
+  var axB = new Buffer(1);
+  var chunkSize = 4;
   var struct = {
     x: stream[0],
     y: stream[1],
@@ -33,6 +25,6 @@ nunchuck.start(function(stream){
     aY: stream[5],
     aZ: stream[6]
   }
-  axStream.put(stream[4]);
-  console.log(stream);
+  //try this http://www.jonathan-petitcolas.com/2013/04/02/create-rotating-cube-in-webgl-with-threejs.html
+  console.log(stream[4],stream[5],stream[6],stream[7],stream[8]);
 });
--- nunchuck-device.js
+++ nunchuck-device.js
@@ -7,6 +7,16 @@ var currentX = center;
 var currentY = center;
 var lastXEvent = "";
 var lastYEvent = "";
+var ZEROG_VOLTAGE=1.65;
+var SENSITIVITY = 0.0188; //478.5mV/g or 0.0188 or 18mg
+var ZERO_AX = 137;
+var ZERO_AY = 135;
+var ZERO_AZ = 175;
+var lastAxValue = ZERO_AX;
+var axIdx = 0;
+var aXAverage = ZERO_AX;
+var aXBuffer = [];
+var mAccel = Math.sqrt(ZERO_AX*ZERO_AX+ZERO_AY*ZERO_AY+ZERO_AZ*ZERO_AZ);
 
 function NunchuckDevice(address, frequency, thresholds){
   this.address = address;
@@ -38,7 +48,7 @@ NunchuckDevice.prototype.start = function(ondata){
     //b = (b ^ 0x17) + 0x17;
     return b;
   }
-  
+
   this.parseData = function(buffer){
     var parsed = new Buffer(7);
     parsed[0] = device.n(buffer[0]); //x
@@ -72,35 +82,29 @@ NunchuckDevice.prototype.start = function(ondata){
     //x -y..etc
     var i = -1;
     var values = [lastXEvent,lastYEvent,'aX','aY','aZ'];
+    var read = [];
     //buffer
     for(i=0; i<buffer.length; i++){
-      value = buffer.readUInt8(i);
-      switch(i){
-        case 0:
-          values[i] = device.decodeX(value);
-          continue;
-        case 1:
-          values[i] = device.decodeY(value);
-          continue;
-        case 2:
-          values[i] = device.decodeC(value);
-          continue;
-        case 3:
-          values[i] = device.decodeZ(value);
-          continue;
-        case 4:
-          values[i] = device.decodeAx(value);
-          continue;
-        case 5:
-          values[i] = device.decodeAy(value);
-          continue;
-        case 6:
-          values[i] = device.decodeAz(value);
-          continue;
-        default:
-          continue;
-      }
+      read[i] = buffer.readUInt8(i);
     }
+
+    values[0] = device.decodeX(read[0]);
+    values[1] = device.decodeY(read[1]);
+    values[2] = device.decodeC(read[2]);
+    values[3] = device.decodeZ(read[3]);
+    var currAx = read[4];
+    var currAy = read[5];
+    var currAz = read[6];
+    //values[4] =  currAx;
+    //values[5] = device.decodeAxAvg(currAx);
+    var motion= device.decodeMotion(currAx,currAy,currAz);
+    values[4] = device.decodeAxG(currAx);
+    values[5] = device.decodeAxAngle(device.decodeAxG(currAx),motion)
+    //values[5] = device.decodeAxAngle2(currAy,currAz);
+    values[6] = device.decodeAx(currAx);
+    values[7] = device.decodeAy(currAy);
+    values[8] = device.decodeAz(currAz);
+
     return values;
   }
 
@@ -112,8 +116,75 @@ NunchuckDevice.prototype.start = function(ondata){
     return newZ == 0?"pressed":"idle";
   }
 
+  this.decodeMotion = function(newX,newY,newZ){
+    var s = newX*newX + newY*newY + newZ*newZ;
+    var mAccelCurrent = Math.sqrt(s);
+    mAccel = (mAccel * 0.9) + (mAccelCurrent * 0.1);
+    return mAccel;
+  }
+
+  this.decodeAxG = function(newAx){
+    var voltsAx	= newAx * 3.3 / 255;
+    var deltaVoltsAx = voltsAx - ZEROG_VOLTAGE;
+    var AXg = deltaVoltsAx / SENSITIVITY;
+    return AXg;
+  }
+
+  //source http://www.starlino.com/imu_guide.html
+  this.decodeAxAngle = function(axg,r){
+    var radians = Math.acos(axg/r)
+    return (radians*180)/3.14;
+  }
+
+  this.decodeAxAngle2 = function(newAy,newAz){
+    var y_val = newAy;
+    var z_val = newAz;
+    var y2 = (y_val*y_val);
+    var z2 = (z_val*z_val);
+    //X Axis
+    var result=Math.sqrt(y2+z2);
+    var radians=Math.atan(result);
+    return (radians*180)/3.14;
+  }
+
+  this.decodeAxAvg = function(newAx){
+    var newAxAvg = aXAverage;
+    aXBuffer[axIdx] = newAx;
+    axIdx++;
+    if(axIdx===10){
+      newAxAvg = avg(aXBuffer);
+      aXAverage = newAxAvg;
+      aXBuffer = [];
+      axIdx = 0;
+    }
+    return aXAverage;
+  }
+
   this.decodeAx= function(newAx){
+    var toRet = "" ;
+    if(newAx === 0){
+        toRet= "tilt-left";
+        lastAxValue = newAx;
+        return toRet;
+    }
+    if(newAx === 255){
+        toRet= "tilt-right";
+        lastAxValue = newAx;
+        return toRet;
+    }
     return newAx;
+    // var diffAx = newAx - lastAxValue;
+    // if(diffAx===0){
+    //   toRet= newAx;
+    // }
+    // if(diffAx<0){
+    //   toRet= diffAx;
+    // }
+    // if(diffAx>0){
+    //   toRet= diffAx;
+    // }
+    // lastAxValue = newAx;
+    //return toRet;
   }
 
   this.decodeAy= function(newAy){
@@ -164,6 +235,12 @@ NunchuckDevice.prototype.start = function(ondata){
 NunchuckDevice.prototype.stop = function(){
   clearInterval(this.started);
   this.bus.closeSync();
+}
+
+function avg(times){
+  var sum = times.reduce(function(a, b) { return a + b; });
+  var avg = sum / times.length;
+  return avg;
 }
 
 module.exports = NunchuckDevice;
