0ddf4203d3460bf5a6b6c33090240243490ec971
Luigi Bifulco <bifulcoluigi@gmail.com>
2016-01-05 10:43:02 +0100

    stream

--- main.js
+++ main.js
@@ -38,6 +38,6 @@ nunchuck.start(function(stream){
     aY: stream[5],
     aZ: stream[6]
   }
-  axStream.put(stream[4],'binary');
-  //console.log(stream);
+  //axStream.put(stream[4],'binary');
+  console.log(stream);
 });
