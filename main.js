e13962274a482a561cf33eb619bfe889a3bdbc29
Luigi Bifulco <bifulcoluigi@gmail.com>
2016-01-05 10:18:24 +0100

    stream buffer
    stream buffer

--- main.js
+++ main.js
@@ -1,24 +1,36 @@
+var streamBuffers = require('stream-buffers');
 var NunchuckDevice = require('./nunchuck-device');
 
-  var NUNCHUCK_ADDRESS = 0x52;
-  var center = 128;
-  var threshholdX = 142-center;
-  var threshholdY = 138-center;
-  var currentX = center;
-  var lastXEvent = "";
+var NUNCHUCK_ADDRESS = 0x52;
+var center = 128;
+var threshholdX = 142-center;
+var threshholdY = 138-center;
+var currentX = center;
+var lastXEvent = "";
 
 
 var nunchuck = new NunchuckDevice(NUNCHUCK_ADDRESS, 1,[threshholdX,threshholdY]);
 nunchuck.init();
-    nunchuck.start(function(stream){
-      var struct = {
-        x: stream[0],
-        y: stream[1],
-        C: stream[2],
-        Z: stream[3],
-        aX: stream[4],
-        aY: stream[5],
-        aZ: stream[6]
-      }
-     console.log(stream);     
-    });
+var readStream = new streamBuffers.ReadableStreamBuffer({
+	frequency: 10,   // in milliseconds.
+	chunkSize: 2048  // in bytes.
+});
+readStream.on('readable', function(data) {
+  var chunk;
+  while((chunk = readStream.read()) !== null) {
+    console.log(chunck);
+  }
+});
+nunchuck.start(function(stream){
+  var struct = {
+    x: stream[0],
+    y: stream[1],
+    C: stream[2],
+    Z: stream[3],
+    aX: stream[4],
+    aY: stream[5],
+    aZ: stream[6]
+  }
+  readStream.put(stream);
+  //console.log(stream);
+});
